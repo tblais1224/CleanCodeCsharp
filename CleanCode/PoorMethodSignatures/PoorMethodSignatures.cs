5f757d749a2eb3d2af4652caf915cd209087bd06
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-10 20:43:07 -0400

    vid 7 done

---
+++ CleanCode/PoorMethodSignatures/PoorMethodSignatures.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CleanCode.PoorMethodSignatures
+{
+    public class PoorMethodSignatures
+    {
+        public void Run()
+        {
+            var userService = new UserService();
+
+            var user = userService.Login("username", "password");
+            var anotherUser = userService.GetUser("username");
+        }
+    }
+
+    public class UserService
+    {
+        private UserDbContext _dbContext = new UserDbContext();
+
+        public User GetUser(string username)
+        {
+            var user = _dbContext.Users.SingleOrDefault(u => u.Username == username);
+            return user;
+        }
+
+        public User Login(string username, string password)
+        {
+            var user = _dbContext.Users.SingleOrDefault(u => u.Username == username && u.Password == password);
+            if (user != null)
+            {
+                user.LastLogin = DateTime.Now;
+            }
+
+            return user;
+        }
+    }
+}
