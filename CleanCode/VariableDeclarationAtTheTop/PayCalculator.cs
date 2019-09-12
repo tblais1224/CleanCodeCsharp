accd5859682a8e7beec51f20dbd214e5f90939b3
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-11 22:11:11 -0400

    vid 10 variable declaration at top done

---
+++ CleanCode/VariableDeclarationAtTheTop/PayCalculator.cs
@@ -0,0 +1,50 @@
+﻿namespace CleanCode.VariableDeclarationAtTheTop
+{
+    class PayCalculator
+    {
+        public decimal CalcGross(decimal rate, decimal hours)
+        {
+            decimal overtimeHours = 0;
+            decimal regularHours = 0;
+
+
+            if (_payFrequency == _payFrequency.Fortnightly)
+            {
+                if (hours > 80)
+                {
+                    overtimeHours = hours - 80;
+                    regularHours = 80;
+                }
+                else
+                {
+                    regularHours = hours;
+                }
+            }
+            else if (_payFrequency == _payFrequency.Weekly)
+            {
+                if (hours > 40)
+                {
+                    overtimeHours = hours - 40;
+                    regularHours = 40;
+                }
+                else
+                {
+                    regularHours = hours;
+                }
+            }
+
+
+            decimal overtimePay = 0;
+
+            if (overtimeHours > 0m)
+            {
+                overtimePay += (rate * 1.5m) * overtimeHours;
+            }
+
+
+            var regularPay = (regularHours * rate);
+            var grossPay = regularPay + overtimePay;
+            return grossPay;
+        }
+    }
+}
