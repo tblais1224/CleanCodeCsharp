f016876c33116072e5ea0217819940ba0a45d166
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-11 21:56:57 -0400

    output params v 9 done

---
+++ CleanCode/OutputParameters/OutputParams.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CleanCode.OutputParameters
+{
+
+    public class GetCustomersResults
+    {
+        public IEnumerable<Customer> Customers { get; set; }
+        public int totalCount { get; set; }
+    }
+    class OutputParams
+    {
+        public void DisplayCustomers()
+        {
+            const int pageIndex = 1;
+            var results = GetCustomers(pageIndex);
+
+            Console.WriteLine("total customers: " + results.totalCount);
+            foreach (var c in results.Customers)
+            {
+                Console.WriteLine(c);
+            }
+
+        }
+
+        public GetCustomersResults GetCustomers(int pageIndex)
+        {
+            var totalCount = 100;
+            return new GetCustomersResults() { Customers = new List<Customer>(), totalCount = totalCount };
+        }
+    }
+}
