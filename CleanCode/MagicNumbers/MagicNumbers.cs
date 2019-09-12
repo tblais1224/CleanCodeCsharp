3d68adad38dc1dc96d85b054a80233fbffbb6590
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-11 22:21:47 -0400

    vid 11 magic numbers is completed

---
+++ CleanCode/MagicNumbers/MagicNumbers.cs
@@ -0,0 +1,30 @@
+﻿namespace CleanCode.MagicNumbers
+{
+    class MagicNumbers
+    {
+        public void ApproveDocument(DocumentStatus status)
+        {
+            if (status == DocumentStatus.Draft)
+            {
+                // ...
+            }
+            else if (status == DocumentStatus.Lodged)
+            {
+                // ...
+            }
+        }
+
+        public void RejectDocument(DocumentStatus status)
+        {
+            switch (status)
+            {
+                case DocumentStatus.Draft:
+                    //...
+                    break;
+                case DocumentStatus.Lodged:
+                    //....
+                    break;
+            }
+        }
+    }
+}
