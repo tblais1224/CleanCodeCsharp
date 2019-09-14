10a6358654cd156c1e1393d9fefc4bf43ec40e2e
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-13 20:59:06 -0400

    vid 12 nested conditionals done

---
+++ CleanCode/NestedConditionals/NestedConditionals.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace CleanCode.NestedConditionals
+{
+    public class Customer
+    {
+        public int LoyaltyPoints { get; set; }
+
+        public bool IsGoldCustomer()
+        {
+            return LoyaltyPoints > 100;
+        }
+    }
+
+    public class Reservation
+    {
+        public Reservation(Customer customer, DateTime dateTime)
+        {
+            From = dateTime;
+            Customer = customer;
+        }
+
+        public DateTime From { get; set; }
+        public Customer Customer { get; set; }
+        public bool IsCanceled { get; set; }
+
+        public void Cancel()
+        {
+            if (IsCancellationPeriodOver())
+                throw new InvalidOperationException("too long to cancel");
+
+            IsCanceled = true;
+        }
+
+
+        private bool IsCancellationPeriodOver()
+        {
+            return (Customer.IsGoldCustomer() && LessThan(24)) || (!Customer.IsGoldCustomer() && LessThan(48));
+        }
+
+        private bool LessThan(int maxHours)
+        {
+            return (From - DateTime.Now).TotalHours < maxHours;
+        }
+    }
+}
