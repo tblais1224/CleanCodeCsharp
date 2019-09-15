30360063ca2ee434e8ab2cb503cff1acaf53d575
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-14 20:04:58 -0400

    vid 16 long methods completed

---
+++ CleanCode/LongMethods/LongMethods.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Data;
+using System.IO;
+
+namespace CleanCode.LongMethods
+{
+    public class DataTableToCsvMapper
+    {
+        public System.IO.MemoryStream Map(DataTable getDataTable)
+        {
+            MemoryStream ReturnStream = new MemoryStream();
+
+            StreamWriter sw = new StreamWriter(ReturnStream);
+            WriteColumnNames(getDataTable, sw);
+            WriteRows(getDataTable, sw);
+            sw.Flush();
+            sw.Close();
+
+            return ReturnStream;
+        }
+
+        private static void WriteRows(DataTable dt, StreamWriter sw)
+        {
+            foreach (DataRow dr in dt.Rows)
+            {
+                WriteRow(dt, dr, sw);
+                sw.WriteLine();
+            }
+        }
+
+        private static void WriteRow(DataTable dt, DataRow dr, StreamWriter sw)
+        {
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                WriteCell(dr, i, sw);
+                WriteSeperatorIfRequired(dt, i, sw);
+            }
+        }
+
+        private static void WriteSeperatorIfRequired(DataTable dt, int i, StreamWriter sw)
+        {
+            if (i < dt.Columns.Count - 1)
+            {
+                sw.Write(",");
+            }
+        }
+
+        private static void WriteCell(DataRow dr, int i, StreamWriter sw)
+        {
+            if (!Convert.IsDBNull(dr[i]))
+            {
+                string str = String.Format("\"{0:c}\"", dr[i].ToString()).Replace("\r\n", " ");
+                sw.Write(str);
+            }
+            else
+            {
+                sw.Write("");
+            }
+        }
+
+        private static void WriteColumnNames(DataTable dt, StreamWriter sw)
+        {
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                sw.Write(dt.Columns[i]);
+                if (i < dt.Columns.Count - 1)
+                {
+                    sw.Write(",");
+                }
+            }
+
+            sw.WriteLine();
+        }
+    }
+
+    public partial class Download : System.Web.UI.Page
+    {
+        private readonly DataTableToCsvMapper _dataTableToCsvMapper = new DataTableToCsvMapper();
+        private readonly TableReader _tableReader = new TableReader();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ClearResponse();
+            SetCacheability();
+            WriteContentToResponse(GetCsv());
+        }
+
+        private byte[] GetCsv()
+        {
+            System.IO.MemoryStream ms = _dataTableToCsvMapper.Map(_tableReader.GetDataTable());
+            byte[] byteArray = ms.ToArray();
+            ms.Flush();
+            ms.Close();
+            return byteArray;
+        }
+
+        private static void WriteContentToResponse(byte[] byteArray)
+        {
+            Response.Charset = System.Text.UTF8Encoding.UTF8.WebName;
+            Response.ContentEncoding = System.Text.UTF8Encoding.UTF8;
+            Response.ContentType = "text/comma-separated-values";
+            Response.AddHeader("Content-Disposition", "attachment; filename=FooFoo.csv");
+            Response.AddHeader("Content-Length", byteArray.Length.ToString());
+            Response.BinaryWrite(byteArray);
+        }
+
+        private static void SetCacheability()
+        {
+            Response.Cache.SetCacheability(HttpCacheability.Private);
+            Response.CacheControl = "private";
+            Response.AppendHeader("Pragma", "cache");
+            Response.AppendHeader("Expires", "60");
+        }
+
+        private static void ClearResponse()
+        {
+            Response.Clear();
+            Response.ClearContent();
+            Response.ClearHeaders();
+            Response.Cookies.Clear();
+        }
+    }
+}
---
+++ CleanCode/LongMethods/TableReader.cs
@@ -0,0 +1,16 @@
+using System.Data;
+using System.Data.SqlClient;
+
+internal class TableReader
+{
+    public DataTable GetDataTable()
+    {
+        string strConn = ConfigurationManager.ConnectionStrings["FooFooConnectString"].ToString();
+        SqlConnection conn = new SqlConnection(strConn);
+        SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM [FooFoo] ORDER BY id ASC", conn);
+        DataSet ds = new DataSet();
+        da.Fill(ds, "FooFoo");
+        DataTable dt = ds.Tables["FooFoo"];
+        return dt;
+    }
+}
